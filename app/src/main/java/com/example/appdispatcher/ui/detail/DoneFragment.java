31cf4ee8b63a77a1a7606b38358a8b96cf54b420
DVG-FAIQOH <faiqoh11.fa@gmail.com>
2020-05-08 17:09:53 +0700

    Recycler View Done Job List

--- app/src/main/java/com/example/appdispatcher/ui/detail/DoneFragment.java
+++ app/src/main/java/com/example/appdispatcher/ui/detail/DoneFragment.java
@@ -1,6 +1,10 @@
 package com.example.appdispatcher.ui.detail;
 
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,13 +12,20 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.appdispatcher.Adapter.JobDoneAdapter;
 import com.example.appdispatcher.R;
+
+import java.util.ArrayList;
 
 public class DoneFragment extends Fragment {
 
     private DoneViewModel mViewModel;
+
+    ArrayList<DoneViewModel> dList = new ArrayList<>();
+    JobDoneAdapter dAdapter;
 
     public static DoneFragment newInstance() {
         return new DoneFragment();
@@ -23,14 +34,33 @@ public class DoneFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_done, container, false);
+        View view = inflater.inflate(R.layout.fragment_done, container, false);
+
+        RecyclerView recyclerViewDoneJobList = view.findViewById(R.id.recyclerViewDone);
+        LinearLayoutManager layoutManagerDoneJobList = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        recyclerViewDoneJobList.setLayoutManager(layoutManagerDoneJobList);
+        fillDatJobPendingList();
+        dAdapter = new JobDoneAdapter(dList);
+        recyclerViewDoneJobList.setAdapter(dAdapter);
+
+        return view;
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(DoneViewModel.class);
-        // TODO: Use the ViewModel
+    private void fillDatJobPendingList() {
+        Resources resources = getResources();
+        String[] arJudul = resources.getStringArray(R.array.title_job_done);
+        String[] arLocation = resources.getStringArray(R.array.location_done);
+        TypedArray a = resources.obtainTypedArray(R.array.drawable_job_done);
+        Drawable[] arFoto = new Drawable[a.length()];
+        for (int i = 0; i < arFoto.length; i++) {
+            arFoto[i] = a.getDrawable(i);
+        }
+        a.recycle();
+
+        for (int i = 0; i < arJudul.length; i++) {
+            dList.add(new DoneViewModel(arJudul[i], arFoto[i], arLocation[i]));
+        }
+        Log.e("dataLog", String.valueOf(dList.size()));
     }
 
 }
