1275adb5019b9e09e0eead9a0f971bf5cde3c9aa
DVG-FAIQOH <faiqoh11.fa@gmail.com>
2020-05-13 15:01:57 +0700

    Dinamis data detail page (pending)

--- app/src/main/java/com/example/appdispatcher/Adapter/JobPendingAdapter.java
+++ app/src/main/java/com/example/appdispatcher/Adapter/JobPendingAdapter.java
@@ -9,28 +9,26 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.appdispatcher.R;
 import com.example.appdispatcher.ui.detail.PendingFragment;
 import com.example.appdispatcher.ui.detail.PendingViewModel;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class JobPendingAdapter extends RecyclerView.Adapter<JobPendingAdapter.ViewHolder> {
 
-    List<PendingViewModel> pendingJobList;
-    PListAdapter pJListAdapter;
+    List<PendingViewModel> pjList;
     private PendingFragment context;
+    PJListAdapter pJListAdapter;
 
-    public JobPendingAdapter(ArrayList<PendingViewModel> pendingJobList) {
-        this.pendingJobList = pendingJobList;
-    }
-
-    public JobPendingAdapter(PendingFragment context, List<PendingViewModel> pList) {
+    public JobPendingAdapter(PendingFragment context, List<PendingViewModel> pjList) {
         super();
+        this.pjList = pjList;
         this.context = context;
         pJListAdapter = context;
     }
+
 
     @NonNull
     @Override
@@ -41,33 +39,36 @@ public class JobPendingAdapter extends RecyclerView.Adapter<JobPendingAdapter.Vi
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        PendingViewModel pendingjoblist = pendingJobList.get(position);
+        PendingViewModel pendingjoblist = pjList.get(position);
         holder.tvJudul.setText(pendingjoblist.judul);
-        holder.ivFoto.setImageDrawable(pendingjoblist.foto);
         holder.tvLocation.setText(pendingjoblist.location);
-    }
-
-    public interface PListAdapter {
-        void doClick(int pos);
+        holder.tvcustomer.setText(pendingjoblist.customer);
+        Glide.with(context).load(pendingjoblist.getFoto()).into(holder.ivFoto);
     }
 
     @Override
     public int getItemCount() {
-        if (pendingJobList != null)
-            return pendingJobList.size();
+        if (pjList != null)
+            return pjList.size();
         return 0;
+    }
+
+    public interface PJListAdapter {
+        void doClick(int pos);
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         ImageView ivFoto;
         TextView tvJudul;
         TextView tvLocation;
+        TextView tvcustomer;
 
         public ViewHolder(View itemView) {
             super(itemView);
             ivFoto = itemView.findViewById(R.id.imageViewlist);
             tvJudul = itemView.findViewById(R.id.textViewJudul);
             tvLocation = itemView.findViewById(R.id.textViewLocation);
+            tvcustomer = itemView.findViewById(R.id.textViewCustomer);
         }
     }
 }
--- app/src/main/java/com/example/appdispatcher/ui/detail/PendingFragment.java
+++ app/src/main/java/com/example/appdispatcher/ui/detail/PendingFragment.java
@@ -2,8 +2,6 @@ package com.example.appdispatcher.ui.detail;
 
 import android.content.Intent;
 import android.content.res.Resources;
-import android.content.res.TypedArray;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,15 +14,27 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.appdispatcher.Adapter.JobPendingAdapter;
 import com.example.appdispatcher.R;
+import com.example.appdispatcher.util.server;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class PendingFragment extends Fragment implements JobPendingAdapter.PListAdapter {
+public class PendingFragment extends Fragment implements JobPendingAdapter.PJListAdapter {
 
     public static final String ID_JOB = "id_job";
-    ArrayList<PendingViewModel> pList = new ArrayList<>();
+    public List<PendingViewModel> pList = new ArrayList<>();
     JobPendingAdapter pAdapter;
 
     public static PendingFragment newInstance() {
@@ -41,27 +51,72 @@ public class PendingFragment extends Fragment implements JobPendingAdapter.PList
         recyclerViewPendingJobList.setLayoutManager(layoutManagerPendingJobList);
         pList.clear();
         fillDatJobPendingList();
-        pAdapter = new JobPendingAdapter(pList);
+        pAdapter = new JobPendingAdapter(this, pList);
         recyclerViewPendingJobList.setAdapter(pAdapter);
 
         return view;
     }
 
     private void fillDatJobPendingList() {
-        Resources resources = getResources();
-        String[] arJudul = resources.getStringArray(R.array.title_job_pending);
-        String[] arLocation = resources.getStringArray(R.array.location_pending);
-        TypedArray a = resources.obtainTypedArray(R.array.drawable_job_pending);
-        Drawable[] arFoto = new Drawable[a.length()];
-        for (int i = 0; i < arFoto.length; i++) {
-            arFoto[i] = a.getDrawable(i);
-        }
-        a.recycle();
+//        Resources resources = getResources();
+//        String[] arJudul = resources.getStringArray(R.array.title_job_pending);
+//        String[] arLocation = resources.getStringArray(R.array.location_pending);
+//        TypedArray a = resources.obtainTypedArray(R.array.drawable_job_pending);
+//        Drawable[] arFoto = new Drawable[a.length()];
+//        for (int i = 0; i < arFoto.length; i++) {
+//            arFoto[i] = a.getDrawable(i);
+//        }
+//        a.recycle();
+//
+//        for (int i = 0; i < arJudul.length; i++) {
+//            pList.add(new PendingViewModel(arJudul[i], arFoto[i], arLocation[i]));
+//        }
+//        Log.e("dataLog", String.valueOf(pList.size()));
 
-        for (int i = 0; i < arJudul.length; i++) {
-            pList.add(new PendingViewModel(arJudul[i], arFoto[i], arLocation[i]));
-        }
-        Log.e("dataLog", String.valueOf(pList.size()));
+        JsonObjectRequest strReq = new JsonObjectRequest(Request.Method.GET, server.getJobListSumm, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.i("response job list", response.toString());
+                JSONObject jObj = response;
+                try {
+                    JSONArray jray = jObj.getJSONArray("job");
+
+                    if (response.length() > 0) {
+                        Resources resources = getResources();
+
+//                        TypedArray a = resources.obtainTypedArray();
+//                        String[] arFoto = new String[a.length()];
+                        for (int i = 0; i < jray.length(); i++) {
+                            JSONObject cat = jray.getJSONObject(i);
+
+                            String url = cat.getJSONObject("category").getString("category_image_url");
+
+//                            String[] arFoto = new String[cat.getJSONObject("category").getString("category_image").length()];
+
+                            PendingViewModel itemCategory = new PendingViewModel();
+                            itemCategory.setJudul(cat.getJSONObject("category").getString("category_name"));
+                            itemCategory.setId_job(cat.getString("id"));
+                            itemCategory.setFoto(cat.getJSONObject("category").getString("category_image_url"));
+                            itemCategory.setCustomer(cat.getJSONObject("customer").getString("customer_name"));
+                            itemCategory.setLocation(cat.getJSONObject("location").getString("long_location"));
+
+                            pList.add(itemCategory);
+                        }
+                        pAdapter.notifyDataSetChanged();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(strReq);
     }
 
     @Override
