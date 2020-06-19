85575fcf4fa04069edb54fd418bcca079f2eb545
DVG-FAIQOH <faiqoh11.fa@gmail.com>
2020-06-19 10:31:22 +0700

    post request item fab

--- app/src/main/java/com/example/appdispatcher/ui/fab/RequestFabFragment.java
+++ app/src/main/java/com/example/appdispatcher/ui/fab/RequestFabFragment.java
@@ -10,10 +10,12 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -32,9 +34,9 @@ import java.util.Map;
 public class RequestFabFragment extends Fragment {
 
     EditText etname, etnominal, etreason;
-    TextView tvIdJob;
+    TextView tvIdJob, tvIdUser;
     Button btn_upload;
-    String name, nominal, reason, id_job;
+    String name, nominal, reason, id_job, id_user;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -50,6 +52,7 @@ public class RequestFabFragment extends Fragment {
         etreason = view.findViewById(R.id.eTextTask3);
         btn_upload = view.findViewById(R.id.btn_upload);
         tvIdJob = view.findViewById(R.id.tv_id_job);
+        tvIdUser = view.findViewById(R.id.tv_id_user);
         fillDetail(id_jobb);
         btn_upload.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,6 +61,7 @@ public class RequestFabFragment extends Fragment {
                 nominal = etnominal.getText().toString().trim();
                 reason = etreason.getText().toString().trim();
                 id_job = tvIdJob.getText().toString().trim();
+                id_user = tvIdUser.getText().toString().trim();
                 submit();
             }
         });
@@ -112,5 +116,62 @@ public class RequestFabFragment extends Fragment {
     }
 
     private void submit() {
+        final JSONObject jobj = new JSONObject();
+        try {
+            jobj.put("id_job", id_job);
+            jobj.put("name_item", name);
+            jobj.put("function_item", reason);
+            jobj.put("price_item", nominal);
+//            jobj.put("id_engineer", id_user);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        final JsonObjectRequest strReq = new JsonObjectRequest(Request.Method.POST, server.request_item_withToken, jobj, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.i("response", response.toString());
+                Toast.makeText(getActivity(), "Successfully :)", Toast.LENGTH_LONG).show();
+
+                /*Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);*/
+//                getActivity().finish();
+
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                        NetworkResponse response = error.networkResponse;
+                        String errorMsg = "";
+                        if (response != null && response.data != null) {
+                            String errorString = new String(response.data);
+                            Log.i("log error", errorString);
+                        }
+                        Toast.makeText(getActivity(), "Error" + error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+
+            /**
+             * Passing some request headers
+             */
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                //headers.put("Content-Type", "application/json");
+                headers.put("Accept", "applicaion/json");
+                // Barer di bawah ini akan di simpan local masing-masing device engineer
+
+//                headers.put("Authorization", "Bearer 14a1105cf64a44f47dd6d53f6b3beb79b65c1e929a6ee94a5c7ad30528d02c3e");
+                SharedPreferences mSetting = getActivity().getSharedPreferences("Setting", Context.MODE_PRIVATE);
+                headers.put("Authorization", mSetting.getString("Token", "missing"));
+                return headers;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(strReq);
     }
 }
