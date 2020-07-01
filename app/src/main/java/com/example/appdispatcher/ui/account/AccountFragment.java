b70bae9ed0b9c5cdac4a0d0b829603b6294863a1
DVG-FAIQOH <faiqoh11.fa@gmail.com>
2020-07-01 11:41:52 +0700

    parsing api account fragment

--- app/src/main/java/com/example/appdispatcher/ui/account/AccountFragment.java
+++ app/src/main/java/com/example/appdispatcher/ui/account/AccountFragment.java
@@ -1,27 +1,65 @@
 package com.example.appdispatcher.ui.account;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
 import com.example.appdispatcher.R;
+import com.example.appdispatcher.util.server;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 public class AccountFragment extends Fragment {
 
     NestedScrollView nestedScrollView;
     BottomNavigationView navigation;
+    public static final String DATE_FORMAT_5 = "dd MMMM yyyy";
+    TextView tvname, tvemail, tvno, tvjobs, tvskill, tvfee, tvdate, tvaddress;
+    ImageView ivuser;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_account, container, false);
         nestedScrollView = root.findViewById(R.id.nestedaccount);
         navigation = getActivity().findViewById(R.id.nav_view);
+
+        tvname = root.findViewById(R.id.text_name);
+        tvemail = root.findViewById(R.id.text_email);
+        tvno = root.findViewById(R.id.text_number);
+        tvjobs = root.findViewById(R.id.textViewtotal);
+        tvskill = root.findViewById(R.id.textViewskill2);
+        tvfee = root.findViewById(R.id.textViewfee2);
+        tvdate = root.findViewById(R.id.textViewdate2);
+        tvaddress = root.findViewById(R.id.textViewaddress2);
+        ivuser = root.findViewById(R.id.ivuser);
 
         nestedScrollView.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
             boolean isNavigationHide = false;
@@ -44,7 +82,58 @@ public class AccountFragment extends Fragment {
             }
         });
 
+        fillaccount();
+
         return root;
+    }
+
+    private void fillaccount() {
+        final SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT_5);
+        final DateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
+        JsonObjectRequest strReq = new JsonObjectRequest(Request.Method.GET, server.getUser_withToken, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONObject jUser = response.getJSONObject("users");
+                    Log.i("users", jUser.toString());
+
+                    tvname.setText(jUser.getString("name"));
+                    tvemail.setText(jUser.getString("email"));
+                    tvno.setText(jUser.getString("phone"));
+                    tvaddress.setText(jUser.getString("address"));
+                    Glide.with(getActivity()).load(jUser.getString("photo_image_url")).into(ivuser);
+                    Date join_date = inputFormat.parse(jUser.getString("created_at"));
+                    tvdate.setText(dateFormat.format(join_date));
+
+                } catch (JSONException | ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }) {
+
+            /**
+             * Passing some request headers
+             */
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                //headers.put("Content-Type", "application/json");
+                headers.put("Accept", "applicaion/json");
+                // Barer di bawah ini akan di simpan local masing-masing device engineer
+
+//                headers.put("Authorization", "Bearer 14a1105cf64a44f47dd6d53f6b3beb79b65c1e929a6ee94a5c7ad30528d02c3e");
+                SharedPreferences mSetting = getActivity().getSharedPreferences("Setting", Context.MODE_PRIVATE);
+                headers.put("Authorization", mSetting.getString("Token", "missing"));
+                return headers;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(strReq);
     }
 
 }
