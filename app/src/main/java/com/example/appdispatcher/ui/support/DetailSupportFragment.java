39a379aaf174369d37f15ceb96c0986558900d51
DVG-FAIQOH <faiqoh11.fa@gmail.com>
2020-07-29 11:09:48 +0700

    progress bar detail support

--- app/src/main/java/com/example/appdispatcher/ui/support/DetailSupportFragment.java
+++ app/src/main/java/com/example/appdispatcher/ui/support/DetailSupportFragment.java
@@ -7,8 +7,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.android.volley.AuthFailureError;
@@ -30,9 +32,10 @@ import java.util.Map;
 
 public class DetailSupportFragment extends Fragment {
 
-    TextView tvproblem, tvreason;
+    TextView tvproblem, tvreason, tvjob;
     ImageView ivfoto;
-    String reason, problem;
+    ProgressBar progressBar;
+    CardView cardView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -40,28 +43,39 @@ public class DetailSupportFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_detail_support, container, false);
 
-        SupportViewModel detail = (SupportViewModel) getActivity().getIntent().getSerializableExtra(SupportFragment.ID_SUPPORT);
-        String id_support = detail.getId_support();
-        fillDetail(id_support);
-
         tvproblem = view.findViewById(R.id.tvproblem);
         tvreason = view.findViewById(R.id.tvreason);
         ivfoto = view.findViewById(R.id.iv_support);
+        progressBar = view.findViewById(R.id.progressBarsupport);
+        cardView = view.findViewById(R.id.cvdesc);
+        tvjob = view.findViewById(R.id.text_view_job);
+
+        SupportViewModel detail = (SupportViewModel) getActivity().getIntent().getSerializableExtra(SupportFragment.ID_SUPPORT);
+        String id_support = detail.getId_support();
+        fillDetail(id_support);
 
         return view;
     }
 
     private void fillDetail(String id_support) {
+//        progressBar.getIndeterminateDrawable().setColorFilter(getResources().getColor(R.color.colorPrimaryDark), PorterDuff.Mode.SRC_IN);
+        progressBar.setVisibility(View.VISIBLE);
+        cardView.setVisibility(View.GONE);
         JsonObjectRequest StrReq = new JsonObjectRequest(Request.Method.GET, server.getdetailsupport_withtoken + "?id_support=" + id_support, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
+                progressBar.setVisibility(View.GONE);
+                cardView.setVisibility(View.VISIBLE);
 
                 try {
-                    JSONObject job = response.getJSONObject("job_support");
+                    JSONObject sup = response.getJSONObject("job_support");
 
-                    tvproblem.setText(job.getString("problem_support"));
-                    tvreason.setText(job.getString("reason_support"));
-                    Glide.with(getActivity()).load(job.getString("picture_support_url")).into(ivfoto);
+                    JSONObject job = sup.getJSONObject("job");
+
+                    tvjob.setText(job.getString("job_name"));
+                    tvproblem.setText(sup.getString("problem_support"));
+                    tvreason.setText(sup.getString("reason_support"));
+                    Glide.with(getActivity()).load(sup.getString("picture_support_url")).into(ivfoto);
 
                 } catch (JSONException e) {
                     e.printStackTrace();
