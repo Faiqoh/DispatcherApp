acebe382e1b1229da18dff4a0ceebc4ab8889afa
DVG-FAIQOH <faiqoh11.fa@gmail.com>
2020-09-23 10:45:07 +0700

    add dialog message for edit profile (name)

--- app/src/main/java/com/example/appdispatcher/ui/account/EditAccountFragment.java
+++ app/src/main/java/com/example/appdispatcher/ui/account/EditAccountFragment.java
@@ -1,11 +1,14 @@
 package com.example.appdispatcher.ui.account;
 
+import android.app.AlertDialog;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -33,8 +36,11 @@ import static android.content.Context.MODE_PRIVATE;
 
 public class EditAccountFragment extends Fragment {
 
-    TextView tvname, tvemail, tvphone, tvaddress;
+    TextView tvname, tvemail, tvphone, tvaddress, tvidUser;
     ImageView ivuser;
+    EditText etName;
+    Button btn_submit, btn_cancel;
+    String id_user;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -46,8 +52,87 @@ public class EditAccountFragment extends Fragment {
         tvphone = view.findViewById(R.id.tvphone2);
         tvaddress = view.findViewById(R.id.tvaddress2);
         ivuser = view.findViewById(R.id.ivuser);
+        tvidUser = view.findViewById(R.id.tv_id_user);
         fillaccount();
+
+        tvname.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(getActivity());
+                View mView = getLayoutInflater().inflate(R.layout.activity_edit_name, null);
+                mBuilder.setView(mView);
+                final AlertDialog dialog = mBuilder.create();
+                dialog.show();
+                fillaccount2();
+
+                btn_submit = mView.findViewById(R.id.btnSubmit);
+                etName = mView.findViewById(R.id.eTextName);
+                btn_cancel = mView.findViewById(R.id.btnCancel);
+
+                btn_cancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+                btn_submit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        id_user = tvidUser.getText().toString().trim();
+                        if (etName.getText().toString().length() == 0) {
+                            etName.setError("Task Should not be empty!");
+                        } else {
+
+                            dialog.dismiss();
+                        }
+                    }
+
+                });
+            }
+        });
         return view;
+    }
+
+    private void fillaccount2() {
+        JsonObjectRequest strReq = new JsonObjectRequest(Request.Method.GET, server.getuserwithToken, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONObject jUser = response.getJSONObject("user");
+                    Log.i("users", jUser.toString());
+
+                    etName.setText(jUser.getString("name"));
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }) {
+
+            /**
+             * Passing some request headers
+             */
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                //headers.put("Content-Type", "application/json");
+                headers.put("Accept", "applicaion/json");
+                // Barer di bawah ini akan di simpan local masing-masing device engineer
+
+//                headers.put("Authorization", "Bearer 14a1105cf64a44f47dd6d53f6b3beb79b65c1e929a6ee94a5c7ad30528d02c3e");
+                SharedPreferences mSetting = getActivity().getSharedPreferences("Setting", MODE_PRIVATE);
+                headers.put("Authorization", mSetting.getString("Token", "missing"));
+                return headers;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(strReq);
     }
 
     private void fillaccount() {
@@ -62,6 +147,7 @@ public class EditAccountFragment extends Fragment {
                     Log.i("users", jUser.toString());
 
                     tvname.setText(jUser.getString("name"));
+                    tvidUser.setText(jUser.getString("id"));
                     tvemail.setText(jUser.getString("email"));
                     tvphone.setText(jUser.getString("phone"));
                     tvaddress.setText(jUser.getString("address"));
