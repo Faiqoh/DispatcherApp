cc7126cc2b27c9194b0be4c26ac83ebd8ee94587
DVG-FAIQOH <faiqoh11.fa@gmail.com>
2020-09-23 11:19:04 +0700

    add dialog message for edit profile (address)

--- app/src/main/java/com/example/appdispatcher/ui/account/EditAccountFragment.java
+++ app/src/main/java/com/example/appdispatcher/ui/account/EditAccountFragment.java
@@ -38,7 +38,7 @@ public class EditAccountFragment extends Fragment {
 
     TextView tvname, tvemail, tvphone, tvaddress, tvidUser;
     ImageView ivuser;
-    EditText etName, etEmail, etPhone;
+    EditText etName, etEmail, etPhone, etAddress;
     Button btn_submit, btn_cancel;
     String id_user;
 
@@ -165,7 +165,83 @@ public class EditAccountFragment extends Fragment {
                 });
             }
         });
+
+        tvaddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(getActivity());
+                View mView = getLayoutInflater().inflate(R.layout.activity_edit_address, null);
+                mBuilder.setView(mView);
+                final AlertDialog dialog = mBuilder.create();
+                dialog.show();
+                fillaccount5();
+
+                btn_submit = mView.findViewById(R.id.btnSubmit);
+                etAddress = mView.findViewById(R.id.eTextAddress);
+                btn_cancel = mView.findViewById(R.id.btnCancel);
+
+                btn_cancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+                btn_submit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        id_user = tvidUser.getText().toString().trim();
+                        if (etAddress.getText().toString().length() == 0) {
+                            etAddress.setError("Phone Should not be empty!");
+                        } else {
+
+                            dialog.dismiss();
+                        }
+                    }
+
+                });
+            }
+        });
         return view;
+    }
+
+    private void fillaccount5() {
+        JsonObjectRequest strReq = new JsonObjectRequest(Request.Method.GET, server.getuserwithToken, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONObject jUser = response.getJSONObject("user");
+                    etAddress.setText(jUser.getString("address"));
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }) {
+
+            /**
+             * Passing some request headers
+             */
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                //headers.put("Content-Type", "application/json");
+                headers.put("Accept", "applicaion/json");
+                // Barer di bawah ini akan di simpan local masing-masing device engineer
+
+//                headers.put("Authorization", "Bearer 14a1105cf64a44f47dd6d53f6b3beb79b65c1e929a6ee94a5c7ad30528d02c3e");
+                SharedPreferences mSetting = getActivity().getSharedPreferences("Setting", MODE_PRIVATE);
+                headers.put("Authorization", mSetting.getString("Token", "missing"));
+                return headers;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(strReq);
     }
 
     private void fillaccount4() {
