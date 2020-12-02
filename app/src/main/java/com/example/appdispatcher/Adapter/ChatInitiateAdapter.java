483ac0452a51b30ad7e04929513225c56c5f003c
Faiqoh <faiqoh11.fa@gmail.com>
2020-12-02 14:07:41 +0700

    parsing json get data initiate chat

---
+++ app/src/main/java/com/example/appdispatcher/Adapter/ChatInitiateAdapter.java
@@ -0,0 +1,95 @@
+package com.example.appdispatcher.Adapter;
+
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.appdispatcher.R;
+import com.example.appdispatcher.ui.support.ChatInitiateModeratorFragment;
+import com.example.appdispatcher.ui.support.SupportFragment;
+import com.example.appdispatcher.ui.support.SupportViewModel;
+
+import java.util.List;
+
+public class ChatInitiateAdapter extends RecyclerView.Adapter<ChatInitiateAdapter.ViewHolder> {
+
+    List<SupportViewModel> sList;
+    private ChatInitiateModeratorFragment context;
+    ChatInitiateModeratorAdapter sListAdapter;
+
+    public ChatInitiateAdapter(ChatInitiateModeratorFragment context, List<SupportViewModel> sList) {
+        super();
+        this.sList = sList;
+        this.context = context;
+        sListAdapter = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_support_history, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        SupportViewModel supportlist = sList.get(position);
+        holder.tvJudul.setText(supportlist.judul);
+//        holder.tvStatus.setText(supportlist.status_support);
+        holder.tvDate.setText(supportlist.date);
+        holder.tvidSupport.setText(supportlist.id_support);
+        Glide.with(context).load(supportlist.getFoto()).into(holder.ivFoto);
+        if (supportlist.getStatus_support().equals("Open")) {
+            holder.tvStatus.setText(supportlist.status_support);
+            holder.tvStatus.setBackgroundColor(Color.parseColor("#e3342f"));
+        } else if (supportlist.getStatus_support().equals("Progress")) {
+            holder.tvStatus.setText(supportlist.status_support);
+            holder.tvStatus.setBackgroundColor(Color.parseColor("#3490dc"));
+        } else if (supportlist.getStatus_support().equals("Done")) {
+            holder.tvStatus.setText(supportlist.status_support);
+            holder.tvStatus.setBackgroundColor(Color.parseColor("#38c172"));
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        if (sList != null)
+            return sList.size();
+        return 0;
+    }
+
+    public SupportViewModel getItem(int pos) {
+        return sList.get(pos);
+    }
+
+    public interface ChatInitiateModeratorAdapter {
+        void doClick(int pos);
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView ivFoto;
+        TextView tvJudul, tvDate, tvidSupport, tvStatus;
+        RelativeLayout head_sub;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivFoto = itemView.findViewById(R.id.imageViewlist);
+            tvJudul = itemView.findViewById(R.id.textViewJudul);
+            tvDate = itemView.findViewById(R.id.textViewDate);
+            head_sub = itemView.findViewById(R.id.head_sub);
+            tvidSupport = itemView.findViewById(R.id.idSupport);
+            tvStatus = itemView.findViewById(R.id.textViewStatus);
+
+        }
+    }
+}
+
--- app/src/main/java/com/example/appdispatcher/ui/support/ChatInitiateModeratorFragment.java
+++ app/src/main/java/com/example/appdispatcher/ui/support/ChatInitiateModeratorFragment.java
@@ -1,22 +1,185 @@
 package com.example.appdispatcher.ui.support;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.appdispatcher.Adapter.ChatInitiateAdapter;
+import com.example.appdispatcher.Adapter.GetSupportAdapter;
 import com.example.appdispatcher.R;
+import com.example.appdispatcher.ui.detail.ScrollingActivityDetailTask;
+import com.example.appdispatcher.util.server;
+import com.facebook.shimmer.ShimmerFrameLayout;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.TimeZone;
 
 
-public class ChatInitiateModeratorFragment extends Fragment {
+public class ChatInitiateModeratorFragment extends Fragment implements ChatInitiateAdapter.ChatInitiateModeratorAdapter{
+
+    public static final String ID_SUPPORT = "id_support";
+    public static final String GET_ID_JOB = "get_id_job";
+    public List<SupportViewModel> sList = new ArrayList<>();
+    ChatInitiateAdapter sAdapter;
+    public static final String DATE_FORMAT_5 = "dd MMMM yyyy";
+    NestedScrollView nestedScrollView;
+    ShimmerFrameLayout shimmerFrameLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat_initiate_moderator, container, false);
+        View view = inflater.inflate(R.layout.fragment_chat_initiate_moderator, container, false);
+
+        nestedScrollView = view.findViewById(R.id.nested_support);
+        shimmerFrameLayout = view.findViewById(R.id.shimmer_view_support);
+
+        RecyclerView rvsupport = view.findViewById(R.id.recyclerViewHistorySupp);
+        LinearLayoutManager lmsupport = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        rvsupport.setLayoutManager(lmsupport);
+        sList.clear();
+        fillData();
+        sAdapter = new ChatInitiateAdapter(this, sList);
+        rvsupport.setAdapter(sAdapter);
+
+        final SwipeRefreshLayout swipeRefreshLayout = view.findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Toast.makeText(getActivity(), "Refresh", Toast.LENGTH_SHORT).show();
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        swipeRefreshLayout.setRefreshing(false);
+                    }
+                }, 2000);
+
+                sList.clear();
+                nestedScrollView.setVisibility(View.GONE);
+                shimmerFrameLayout.startShimmerAnimation();
+                fillData();
+
+            }
+        });
+
+        return view;
+    }
+
+    private void fillData() {
+        final SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT_5);
+        final DateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US);
+        dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+        JsonObjectRequest strReq = new JsonObjectRequest(Request.Method.GET, server.getsupport_withtoken, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                nestedScrollView.setVisibility(View.VISIBLE);
+                shimmerFrameLayout.stopShimmerAnimation();
+                shimmerFrameLayout.setVisibility(View.GONE);
+                Log.i("response job list", response.toString());
+                JSONObject jObj = response;
+                try {
+                    JSONArray jray = jObj.getJSONArray("job_support");
+                    if (response.length() > 0) {
+                        if (sList != null) {
+                            sList.clear();
+                        } else {
+                            sList = new ArrayList<>();
+                        }
+
+                        for (int i = 0; i < jray.length(); i++) {
+                            JSONObject sup = jray.getJSONObject(i);
+
+                            Date date_add = inputFormat.parse(sup.getString("date_add"));
+
+                            SupportViewModel itemCategory = new SupportViewModel();
+                            itemCategory.setId_support(sup.getString("id"));
+                            itemCategory.setJudul(sup.getJSONObject("job").getString("job_name"));
+                            itemCategory.setStatus_support(sup.getString("status"));
+                            itemCategory.setDate(dateFormat.format(date_add));
+                            itemCategory.setFoto(sup.getJSONObject("job_category").getString("category_image_url"));
+                            sList.add(itemCategory);
+                        }
+                        sAdapter.notifyDataSetChanged();
+                    }
+                } catch (JSONException | ParseException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                //headers.put("Content-Type", "application/json");
+                headers.put("Accept", "applicaion/json");
+                // Barer di bawah ini akan di simpan local masing-masing device engineer
+
+//                headers.put("Authorization", "Bearer 14a1105cf64a44f47dd6d53f6b3beb79b65c1e929a6ee94a5c7ad30528d02c3e");
+                SharedPreferences mSetting = getActivity().getSharedPreferences("Setting", Context.MODE_PRIVATE);
+                headers.put("Authorization", mSetting.getString("Token", "missing"));
+                return headers;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(strReq);
+    }
+
+    @Override
+    public void doClick(int pos) {
+        Intent intent = new Intent(getContext(), ScrollingActivityDetailTask.class);
+        intent.putExtra(ID_SUPPORT, sAdapter.getItem(pos));
+        intent.putExtra(GET_ID_JOB, "id_support");
+        startActivity(intent);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        shimmerFrameLayout.startShimmerAnimation();
+    }
+
+    @Override
+    public void onPause() {
+        shimmerFrameLayout.stopShimmerAnimation();
+        super.onPause();
     }
 }
